34a3a28a4b82e0fd16a235bfbd6091b803834328
boolayinka <boolayinka@gmail.com>
2023-02-21 14:40:12 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet in lower case
+ *
+ * Return: always 0;
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}

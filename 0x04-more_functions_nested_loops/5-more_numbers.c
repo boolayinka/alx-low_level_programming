b269dc73f6cd3a8caf0fd4b7fba993a6041af803
boolayinka <boolayinka@gmail.com>
2023-02-23 14:12:06 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,12 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; j < 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 	for (j = 0; j <= 14; j++)
 	{
-	if (j > 0)
-	{
-	_putchar((j / 10) + '0');
-	}
+	if (j >= 10)
+		_putchar('1');
 	_putchar((j % 10) + '0');
 	}
 	_putchar('\n');

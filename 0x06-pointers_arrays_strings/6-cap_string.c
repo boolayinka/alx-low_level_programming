f163e08ab45710e26537f157572c1f12499a3517
boolayinka <boolayinka@gmail.com>
2023-03-01 16:40:40 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-		while (!(str[index] >= 'a' && str[index] <= '2'))
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
 		if (str[index - 1] == ' ' ||

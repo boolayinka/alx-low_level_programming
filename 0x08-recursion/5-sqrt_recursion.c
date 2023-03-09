dff0718084cc62ec89a62e1a9b55e80f34bafeef
boolayinka <boolayinka@gmail.com>
2023-03-08 16:15:21 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,14 +8,34 @@
 
 int _evaluate(int i, int n)
 {
-	if (n == 0 || n == 1)
-		return (n);
+if (n == 0 || n == 1)
+return (n);
 
-	else if (i * i < n)
-		return (_evaluate(i + 1, n));
+else if (i * i < n)
+return (_evaluate(i + 1, n));
 
-	else if (i * i == n)
+else if (i * i == n)
 return (i);
+
 return (-1);
+
 return (-1);
+}
+/*
+ * *_sqrt_recursion - evaluate sqrt
+ * *@n: integer
+ *  *Return: Sgrt_recursion
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i < 0)
+		return (-1);
+
+	else
+	{
+		return (_evaluate(i, n));
+	}
 }

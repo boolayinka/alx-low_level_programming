e7adae39cee4ed14bbf09cd4720f8958bd01bec9
boolayinka <boolayinka@gmail.com>
2023-03-23 05:12:43 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -4,12 +4,12 @@
 
 /**
  * print_strings - Prints strings, follwed by a new line
- * @separtor: The string to be printed between strings.
+ * @separator: The string to be printed between strings.
  * @n: The number of strings passed to the function.
  * @...: A variable number of strings to be printed.
  *
  * Description: If separator is NULL, it is not printed.
- * 	If one of the strings if NULL, (nil) is printed instead.
+ * If one of the strings if NULL, (nil) is printed instead.
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {

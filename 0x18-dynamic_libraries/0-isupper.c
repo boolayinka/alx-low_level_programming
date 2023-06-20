5c30343a203863dca2c9dd15c2cbca53c899e9e2
Boolayinka <boolayinka@gmail.com>
2023-06-20 14:21:08 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -2,16 +2,14 @@
 
 /**
  * _isupper - uppercase letters
- * @c: char to check
+ * @c: char t check
  *
- * Prince Solomon
  * Return: 0 or 1
  */
 
 int _isupper(int c)
-
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
--- 0x18-dynamic_libraries/0-strcat.c
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -1,27 +1,22 @@
 #include "main.h"
 
 /**
- * _strcat - concat 2 string
- * @dest:char
- * @src:char
- * Return:char
+ * _strcat - Concatenates the string pointed to by @src
+ * null byte, to the end of the string pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ *
+ * Return: A pointer to the destination string @dest.
  */
-
 char *_strcat(char *dest, char *src)
 {
-	char *s = dest;
+	int index = 0, dest_len = 0;
 
-	while (*dest != '\0')
-	{
-		dest++;
-	}
+	while (dest[index++])
+		dest_len++;
 
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-	return (s);
-} /*Prince Solomon*/
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}

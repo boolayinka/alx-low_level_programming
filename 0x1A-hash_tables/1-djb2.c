2a40d5d66a8de4eb5b917384bc27383e2cae91ec
Boolayinka <boolayinka@gmail.com>
2023-07-06 17:03:12 -0700

    0x1A-hash_tables

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,4 +1,10 @@
-unsigned long int hash_djb2(const unsigned char *str)
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
+`:wunsigned long int hash_djb2(const unsigned char *str)
 {
     unsigned long int hash;
     int c;
